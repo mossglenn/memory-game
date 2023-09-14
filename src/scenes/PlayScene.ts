e7e2b0b266d5db6cf2e29e77ac19d06b1b7e437e
Amos Glenn <amos@amosglenn.com>
2023-09-14 15:01:00 -0400

    reproduce bug with images and planes

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,12 +1,14 @@
-import { CardKey } from '../../AssetKeys.ts';
-import Card from '../../Card.ts';
-import { GameSettings } from '../../gameSettings.ts';
+export enum CardKey {
+  BACK = 'back',
+  FRONT = 'front',
+  ONE = 'card1',
+  TWO = 'card2',
+  THREE = 'card3',
+  FOUR = 'card4'
+}
 
 export default class PlayScene extends Phaser.Scene {
-  cards: Card[] = [];
-
-  tests: Phaser.GameObjects.Image[] = [];
-
+  images: Phaser.GameObjects.Image[] = [];
   planes: Phaser.GameObjects.Plane[] = [];
 
   constructor() {
@@ -14,54 +16,35 @@ export default class PlayScene extends Phaser.Scene {
   }
 
   create() {
-    this.tests.push(this.add.image(0, -2000, CardKey.ONE));
-    this.tests.push(this.add.image(0, -2000, CardKey.TWO));
-    this.tests.push(this.add.image(0, -2000, CardKey.THREE));
-    this.tests.push(this.add.image(0, -2000, CardKey.FOUR));
+    this.images.push(this.add.image(0, 0, CardKey.ONE));
+    this.images.push(this.add.image(0, 0, CardKey.TWO));
+    this.images.push(this.add.image(0, 0, CardKey.THREE));
+    this.images.push(this.add.image(0, 0, CardKey.FOUR));
 
-    Phaser.Actions.GridAlign(this.tests, {
+    Phaser.Actions.GridAlign(this.images, {
       width: 2,
       height: 2,
-      cellHeight: 300,
-      cellWidth: 300,
+      cellHeight: 200,
+      cellWidth: 200,
       x: 100,
       y: 100
     });
 
-    this.planes.push(this.add.plane(0, -2000, CardKey.ONE));
-    this.planes.push(this.add.plane(0, -2000, CardKey.TWO));
-    this.planes.push(this.add.plane(0, -2000, CardKey.THREE));
-    this.planes.push(this.add.plane(0, -2000, CardKey.FOUR));
+    this.planes.push(this.add.plane(0, 0, CardKey.ONE));
+    this.planes.push(this.add.plane(0, 0, CardKey.TWO));
+    this.planes.push(this.add.plane(0, 0, CardKey.THREE));
+    this.planes.push(this.add.plane(0, 0, CardKey.FOUR));
 
     Phaser.Actions.GridAlign(this.planes, {
       width: 2,
       height: 2,
-      cellHeight: 300,
-      cellWidth: 300,
-      x: 100,
-      y: 100
+      cellHeight: 200,
+      cellWidth: 200,
+      x: 600,
+      y: 300
     });
 
-    this.cards.push(new Card(CardKey.FOUR, this, 800, 100));
-    this.cards.push(new Card(CardKey.THREE, this, 800, 300));
-    this.cards.push(new Card(CardKey.TWO, this, 800, 500));
-    this.cards.push(new Card(CardKey.ONE, this, 800, 700));
-    console.log(JSON.stringify(this.cards[1]));
-
-    console.log(Object.hasOwn(this.cards[0], 'x'));
-    console.log(Object.hasOwn(this.cards[0], 'y'));
-
-    Phaser.Actions.GridAlign(this.cards, {
-      width: 2,
-      height: 2,
-      cellHeight: 300,
-      cellWidth: 300,
-      x: 100,
-      y: 100
-    });
-
-    console.log(JSON.stringify(this.cards[0]));
-    console.log(JSON.stringify(this.tests[0]));
+    console.log(JSON.stringify(this.images[0]));
     console.log(JSON.stringify(this.planes[0]));
   }
 }
